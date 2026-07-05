8a2bfc07c38fc0009cd469c7f1fa954cd96a9162
EngMohamed95 <eng.mhamed99@gmail.com>
2026-07-05 15:41:37 +0300

    Update default Firebase config credentials in source files to point to lawyer-sa

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAWYN_nG9tCYIG3_DfpumMvDnaS9YI_QlU",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "lawyer-7c506.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "lawyer-7c506",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "lawyer-7c506.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "223439278921",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:223439278921:web:7510b793d99a4697364098",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-6EWPNR7Q2V"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDsWX_Lp3FSQVx1zpYSrdlFvKX0AC8gc7U",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "lawyer-sa.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "lawyer-sa",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "lawyer-sa.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "755610160641",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:755610160641:web:8aeb37e010f0b16b437a00",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-NCMRZD30V0"
 };
 
 // Initialize Firebase
